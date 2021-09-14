f34315534368824726e4bd0e4634a6b654ff62a0
Sara12q2 <67774856+Sara12q2@users.noreply.github.com>
2021-09-14 11:47:51 -0500

    Basico con detalles

--- src/main/java/Concatenacion.java
+++
@@ -1,9 +0,0 @@
-
-import AnalizadorLexico.ventanaConcatenacion;
-
-public class Concatenacion {
-    public static void main(String[] args) {
-        ventanaConcatenacion uno = new ventanaConcatenacion();
-        uno.opciones();
-    }    
-}
